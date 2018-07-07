98473c09a8594508951f6a6f7d1bd92947fb663e
nacdevs <40808693+nacdevs@users.noreply.github.com>
2018-07-06 22:00:36 -0300

    Ejercicios 1,2,y3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,24 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var precioConDescuento;
+	var precioFinal;
+	var iva;
+
+		
+
+	precio=prompt("Ingrese el precio");
+	descuento = prompt("Ingrese el descuento");
+
+	precio=parseInt(precio);
+	descuento=parseInt(descuento); 
+
+	precioConDescuento= precio - (descuento * 10/100);
+	iva= precioConDescuento*0.21;
+	precioFinal= precioConDescuento +  iva;
+
+	alert("El precio es: "+precio+", el descuento es: "+descuento+", el iva es: "+iva);
+	document.getElementById('elPrecioFinal').value=precioConDescuento;
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,21 @@
 
 function mostrar()
 {
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
+
+	base= prompt("Introduzca la base");
+	altura= prompt("Introduzca la altura");
+
+	base=parseInt(base);
+	altura=parseInt(altura);
+
+	superficie= (base*altura)/2;
+	perimetro= (base*3);
+
+	alert("La superficie es "+superficie+" y el perimetro es "+perimetro);
+
 
 }
