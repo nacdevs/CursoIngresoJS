a1e44137ee336b27efdc4245344264911c09c37f
nacdevs <40808693+nacdevs@users.noreply.github.com>
2018-07-06 22:09:23 -0300

    Tres corregido y andando

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -14,11 +14,11 @@ function mostrar()
 	precio=parseInt(precio);
 	descuento=parseInt(descuento); 
 
-	precioConDescuento= precio - (descuento * 10/100);
+	precioConDescuento= precio - (precio*(descuento * (1/100)));
 	iva= precioConDescuento*0.21;
 	precioFinal= precioConDescuento +  iva;
 
 	alert("El precio es: "+precio+", el descuento es: "+descuento+", el iva es: "+iva);
-	document.getElementById('elPrecioFinal').value=precioConDescuento;
+	document.getElementById('elPrecioFinal').value=precioFinal;
 
 }
