e8cb9e021888f805e69ec568dcc1f90b2f53b5d5
nacdevs <40808693+nacdevs@users.noreply.github.com>
2018-07-06 22:19:22 -0300

    Actualizado uno con un nuevo ejercicio

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,7 +1,12 @@
+/*
+Ejercicio de parcial de practica:
+Se ingresan tres numeros, se muestra la suma de los tres, el promedio de los tres y la resta de los tres en unico alert
 
+
+*/
 function mostrar()
 {
-	var base;
+	/*var base;
 	var altura;
 	var superficie;
 	var perimetro;
@@ -15,7 +20,27 @@ function mostrar()
 	superficie= (base*altura)/2;
 	perimetro= (base*3);
 
-	alert("La superficie es "+superficie+" y el perimetro es "+perimetro);
+	alert("La superficie es "+superficie+" y el perimetro es "+perimetro);*/
 
+	var numero1;
+	var numero2;
+	var numero3;
+	var suma;
+	var promedio;
+	var resta;
+
+	numero1=prompt("Ingrese numero uno");
+	numero2=prompt("Ingrese numero dos");
+	numero3=prompt("Ingrese numero tres");
+
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
+	numero3=parseInt(numero3);
+
+	suma= numero1+numero2+numero3;
+	resta= numero1-numero2-numero3;
+	promedio = suma/3;
+
+	alert("La suma es "+suma+" la resta es "+resta+" y el promedio es "+promedio);
 
 }
