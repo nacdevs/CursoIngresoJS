eddb4b4214f58bb5ed23b48a2974bd658acbf7f4
nacdevs <40808693+nacdevs@users.noreply.github.com>
2018-07-10 22:08:01 -0300

    done!

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,7 +1,17 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad;
+	var estCivil;
+	estCivil=document.getElementById('estadoCivil').value;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
 
+	if(edad<18 && estCivil!="Soltero"){
+		alert('Es muy pequeño para NO ser soltero');
+	}else{
+
+	}
 	
 
 
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,7 +1,20 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+	var estCivil;
+	estCivil=document.getElementById('estadoCivil').value;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
 
+	if(edad<18 && estCivil!="Soltero"){
+		
+	}else{
+		if(estCivil=="Soltero" && edad>=18){
+			alert('Es soltero y no es menor')
+
+			}
+		}
 	
 
 
