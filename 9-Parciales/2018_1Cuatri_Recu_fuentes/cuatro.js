267b635e3b6f0cf112956c5c37aec6495617eb3c
nacdevs <40808693+nacdevs@users.noreply.github.com>
2018-07-13 21:28:09 -0300

    done

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,30 @@
 function mostrar()
 {
+	var numA;
+	var numB;
+	var suma;
+
+	numA=prompt("ingrese el primer numero");
+	numB=prompt("ingrese el segundo numero");
+	
+  
+	if(numA==numB){
+		alert(numA+numB);
+	}
+
+	numA=parseInt(numA);
+	numB=parseInt(numB);
+
+	if(numA>numB){		
+		alert(numA/numB);
+	}else{
+		suma=numA+numB
+		if(50>suma){
+			alert("la suma es : "+ suma + " y es menor a 50");
+		}
+	}
+
+
+
 
 }
