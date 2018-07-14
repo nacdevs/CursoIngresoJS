ce4a03b205d940a8386cd8a0363d4f87b34ce576
nacdevs <40808693+nacdevs@users.noreply.github.com>
2018-07-13 21:29:53 -0300

    Hechos

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,29 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+//alert (mesDelAño);
+
+switch(mesDelAño){
+
+		case "Enero":		
+		case "Febrero":	
+		case "Marzo":	
+		case "Abril":
+		case "Mayo":		
+		case "Junio":
+			alert("Falta para el invierno."  );
+			break;
+
+		case "Julio":		
+		case "Agosto":
+			alert("Abrigate que hace frio." );
+			break;						
+
+		default:
+			alert("Ya pasamos el frio, ahora calor!!!." );
+			break;
+}
+
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,8 +5,24 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
+	switch(mesDelAño){
+
+		case "Febrero":
+			alert("tiene 28 días.");
+			break; 
+
+		case "Noviembre":
+		case "Abril":
+		case "Junio":
+		case "Septiembre":		
+			alert("tiene 30 días.");
+			break;
+		
+		default:
+			alert("tiene 31 dias")		
+}
 	
 
 
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN	
