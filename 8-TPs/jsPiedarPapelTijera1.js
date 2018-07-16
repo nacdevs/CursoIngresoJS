774fa7eb5d141db30ac86b970e1036ef46c4508a
nacdevs <40808693+nacdevs@users.noreply.github.com>
2018-07-15 21:49:30 -0300

    Se que lees mis commits muchacho asique ñam ñam ñam
    In other words done

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -1,29 +1,66 @@
-/*Al comenzar el juego generaremos un número 
-RANDOM del 1 al 3 para la selección de la máquina, 
+/*Al comenzar el juego generaremos un número
+RANDOM del 1 al 3 para la selección de la máquina,
 siendo 1 para “piedra”, el 2 para “papel” y 3 para “tijera”.
-El jugador seleccionará una imagen correspondiente 
+El jugador seleccionará una imagen correspondiente
 a su opción  y le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
 
 function comenzar()
 {
-	
+	eleccionMaquina=Math.floor((Math.random() * 3) + 1);
+	alert(eleccionMaquina);
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
+	switch (eleccionMaquina) {
+			case 1:
+				alert("empato");
+			break;
+			case 2:
+				alert("perdio")
+				break;
+			case 3:
+				alert("gano")
+				break;
+
+
+	}
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
+	switch (eleccionMaquina) {
+			case 1:
+				alert("gano");
+			break;
+			case 2:
+				alert("empato")
+				break;
+			case 3:
+				alert("perdio")
+				break;
 
+
+	}
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	switch (eleccionMaquina) {
+			case 1:
+				alert("perdio");
+			break;
+			case 2:
+				alert("gano")
+				break;
+			case 3:
+				alert("empato")
+				break;
+
+
+	}
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/jsPiedarPapelTijera2.js
+++ 8-TPs/jsPiedarPapelTijera2.js
@@ -2,30 +2,90 @@ var eleccionMaquina;
 var ContadorDeEmpates=0;
 var ContadorDeGanadas=0;
 var ContadorDePerdidas=0;
+var GanadasBox;
+var PerdidasBox;
+var EmpatadasBox;
+var mensaje;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 3
-	 	
+	eleccionMaquina=Math.floor((Math.random() * 3) + 1);
+	//alert(eleccionMaquina);
 
+	GanadasBox=document.getElementById('ganadas');
+	PerdidasBox=document.getElementById('perdidas');
+	EmpatadasBox=document.getElementById('empatadas');
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
+	switch (eleccionMaquina) {
+			case 1:
+				mensaje="empato";
+				ContadorDeEmpates++;
+			break;
+			case 2:
+				mensaje="perdio";
+				ContadorDePerdidas++;
+				break;
+			case 3:
+				mensaje="gano";
+				ContadorDeGanadas++;
+				break;
+
+}
+alert(mensaje);
+mostrarResultado();
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
+	switch (eleccionMaquina) {
+			case 1:
+				mensaje="gano";
+				ContadorDeGanadas++;
+			break;
+			case 2:
+				mensaje="empato";
+				ContadorDeEmpates++;
+				break;
+			case 3:
+				mensaje="perdio";
+				ContadorDePerdidas++;
+				break;
+
+	}
+alert(mensaje);
+mostrarResultado();
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	switch (eleccionMaquina) {
+			case 1:
+				mensaje="perdio";
+				ContadorDePerdidas++;
+			break;
+			case 2:
+				mensaje="gano";
+				ContadorDeGanadas++;
+				break;
+			case 3:
+				mensaje="empato";
+				ContadorDeEmpates++
+				break;
+	}
+alert(mensaje);
+mostrarResultado();
+
 }//FIN DE LA FUNCIÓN
 
 function mostrarResultado()
 {
+	GanadasBox.value=ContadorDeGanadas;
+	PerdidasBox.value=ContadorDePerdidas;
+	EmpatadasBox.value=ContadorDeEmpates;
 
 }
